2d0d0f7f54da1c38183e57c4adfb0b978d26e572
neko3141592 <yudai3.1415926@gmail.com>
2026-09-09 16:22:50 +0900

    マスコンの作成

--- Assets/Nakatetsu/Train/Tests/EditMode/MasterControllerLogicTests.cs
+++ Assets/Nakatetsu/Train/Tests/EditMode/MasterControllerLogicTests.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using UnityEngine;
+using Nakatetsu.Train.Consist;
+using Nakatetsu.Train.Equipment;
 using Nakatetsu.Train.Operation;
 
 namespace Nakatetsu.Train.Tests
@@ -84,6 +87,81 @@ namespace Nakatetsu.Train.Tests
 
             Assert.That(context.State.powerPosition, Is.Zero);
             Assert.That(context.State.brakePosition, Is.EqualTo(8));
+        }
+    }
+
+    public sealed class TrainEquipmentBuilderTests
+    {
+        [Test]
+        public void BuildsEquipmentUnderCarsAndAssignsZeroBasedCarIndexes()
+        {
+            GameObject equipmentsObject = null;
+            GameObject masterControllerPrefab = null;
+            GameObject brakePrefab = null;
+            ConsistDefinitionAsset consistDefinition = null;
+            CarDefinitionAsset cabDefinition = null;
+            CarDefinitionAsset trailerDefinition = null;
+
+            try
+            {
+                equipmentsObject = new GameObject("Equipments");
+                Transform common = new GameObject("Common").transform;
+                common.SetParent(equipmentsObject.transform, false);
+                Transform tims = new GameObject("TIMS").transform;
+                tims.SetParent(common, false);
+
+                masterControllerPrefab = new GameObject("MasterController");
+                masterControllerPrefab.AddComponent<MasterController>();
+                brakePrefab = new GameObject("BrakeEquipment");
+
+                cabDefinition = ScriptableObject.CreateInstance<CarDefinitionAsset>();
+                cabDefinition.masterControllerPrefab = masterControllerPrefab;
+                cabDefinition.brakeEquipmentPrefab = brakePrefab;
+                trailerDefinition = ScriptableObject.CreateInstance<CarDefinitionAsset>();
+                trailerDefinition.brakeEquipmentPrefab = brakePrefab;
+
+                consistDefinition = ScriptableObject.CreateInstance<ConsistDefinitionAsset>();
+                consistDefinition.cars.Add(cabDefinition);
+                consistDefinition.cars.Add(trailerDefinition);
+
+                TrainEquipmentBuilder builder =
+                    equipmentsObject.AddComponent<TrainEquipmentBuilder>();
+                builder.Configure(consistDefinition);
+
+                Assert.That(builder.Build(), Is.True);
+                Assert.That(equipmentsObject.transform.Find("Common/TIMS"), Is.Not.Null);
+                Assert.That(equipmentsObject.transform.Find("Cars/Car 1/MasterController"), Is.Not.Null);
+                Assert.That(equipmentsObject.transform.Find("Cars/Car 2/BrakeEquipment"), Is.Not.Null);
+
+                TrainEquipmentAssignment[] assignments =
+                    equipmentsObject.GetComponentsInChildren<TrainEquipmentAssignment>(true);
+                Assert.That(assignments.Length, Is.EqualTo(3));
+                Assert.That(
+                    equipmentsObject.transform
+                        .Find("Cars/Car 1/MasterController")
+                        .GetComponent<TrainEquipmentAssignment>()
+                        .AssignedCarIndex,
+                    Is.Zero);
+                Assert.That(
+                    equipmentsObject.transform
+                        .Find("Cars/Car 2/BrakeEquipment")
+                        .GetComponent<TrainEquipmentAssignment>()
+                        .AssignedCarIndex,
+                    Is.EqualTo(1));
+
+                Assert.That(builder.Build(), Is.True);
+                assignments = equipmentsObject.GetComponentsInChildren<TrainEquipmentAssignment>(true);
+                Assert.That(assignments.Length, Is.EqualTo(3));
+            }
+            finally
+            {
+                Object.DestroyImmediate(equipmentsObject);
+                Object.DestroyImmediate(masterControllerPrefab);
+                Object.DestroyImmediate(brakePrefab);
+                Object.DestroyImmediate(consistDefinition);
+                Object.DestroyImmediate(cabDefinition);
+                Object.DestroyImmediate(trailerDefinition);
+            }
         }
     }
 }
