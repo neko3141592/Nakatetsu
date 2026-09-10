0227a1045b27430040fb229ac07e38dace895a5a
neko3141592 <yudai3.1415926@gmail.com>
2026-09-10 09:23:21 +0900

    ブレーキシリンダーの仕様定義を追加

--- Assets/Nakatetsu/Train/Brake/Cylinder/Scripts/BrakeCylinderContext.cs
+++ Assets/Nakatetsu/Train/Brake/Cylinder/Scripts/BrakeCylinderContext.cs
@@ -9,8 +9,18 @@ namespace Nakatetsu.Train.Brake.Cylinder
         public bool isHealthy = true;
     }
 
+    public sealed class BrakeCylinderSettings
+    {
+        public float maximumPressureKPa = 500f;
+        public float applyRateKPaPerSecond = 250f;
+        public float releaseRateKPaPerSecond = 350f;
+        public float pistonAreaM2 = 0.01f;
+        public float mechanicalEfficiency = 0.9f;
+    }
+
     public sealed class BrakeCylinderContext
     {
+        public BrakeCylinderSettings Settings { get; } = new();
         public BrakeCylinderState State { get; } = new();
     }
 }
