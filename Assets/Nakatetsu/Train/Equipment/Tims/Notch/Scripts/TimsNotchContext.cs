5d7b819b3998a8ae0b7da49e2e53e509691a6678
neko3141592 <yudai3.1415926@gmail.com>
2026-09-14 21:53:55 +0900

    TimsNotchControllerの追加

--- Assets/Nakatetsu/Train/Equipment/Tims/Notch/Scripts/TimsNotchContext.cs
+++ Assets/Nakatetsu/Train/Equipment/Tims/Notch/Scripts/TimsNotchContext.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 
 namespace Nakatetsu.Train.Equipment.Tims.Notch
 {
-    public enum TimsCabSelection { Forward = 0, Reverse = 1 }
-    public enum TimsReverserPosition { Reverse = -1, Neutral = 0, Forward = 1 }
 
     public struct TimsCabInput
     {
-        public bool hasSelection;
-        public TimsCabSelection selection;
         public int powerNotch;
         public int brakeNotch;
-        public TimsReverserPosition reverserPosition;
     }
 
     public sealed class TimsNotchInput
@@ -20,7 +15,13 @@ namespace Nakatetsu.Train.Equipment.Tims.Notch
         // Set after the caller has resolved the consist and decoded all available cab inputs.
         public bool isReady;
         public readonly List<TimsCabInput> cars = new();
+
+        // ATC
         public int atcBrakeStep;
+
+        // ATO/TASC
+        public int tascBrakeStep;
+        public int atoPowerNotch;
     }
 
     public sealed class TimsNotchSettings
@@ -30,17 +31,12 @@ namespace Nakatetsu.Train.Equipment.Tims.Notch
 
     public sealed class TimsNotchOutput
     {
-        public int activatedCabIndex = -1;
         public bool isEmergencyBrakeRequested = true;
         public int manualPowerNotch;
         public int manualBrakeStep;
-        public int atcBrakeStep;
         public int resolvedPowerNotch;
         public int resolvedBrakeStep;
-        public int consistForceSign;
-        public TimsReverserPosition reverserPosition;
         public string manualBrakeStepLabel = "B0-0";
-        public string atcBrakeStepLabel = "B0-0";
         public string brakeStepLabel = "B0-0";
         public string resolvedNotchLabel = "N";
     }
