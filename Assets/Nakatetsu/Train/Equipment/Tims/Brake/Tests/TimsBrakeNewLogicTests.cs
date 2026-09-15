ba20e24362d726914204f01510fcce498965856f
neko3141592 <yudai3.1415926@gmail.com>
2026-09-16 08:56:00 +0900

    NewLogicの回生目標をVVVF搭載車へ均等配分

--- Assets/Nakatetsu/Train/Equipment/Tims/Brake/Tests/TimsBrakeNewLogicTests.cs
+++ Assets/Nakatetsu/Train/Equipment/Tims/Brake/Tests/TimsBrakeNewLogicTests.cs
@@ -44,6 +44,55 @@ namespace Nakatetsu.Train.Equipment.Tims.Tests
             Assert.That(context.Workspace.targetCarBrakeForcesN, Is.EqualTo(new[] { 0f, 0f }));
         }
 
+        [Test]
+        public void Calculate_DistributesRegenEquallyToMotorCarsRegardlessOfMass()
+        {
+            TimsBrakeContext context = CreateContext();
+            context.Input.cars[0].isVvvfMotorCar = true;
+            context.Input.cars[1].isVvvfMotorCar = true;
+            context.Input.cars.Insert(1, new TimsBrakeCarInput
+            {
+                massKg = 20000f,
+                airForcePerKPa = 100f,
+                isTrailerCar = true
+            });
+
+            TimsBrakeNewLogic.Calculate(context);
+
+            Assert.That(context.Workspace.remainingTargetBrakeForceN, Is.EqualTo(88000f));
+            Assert.That(context.Workspace.targetRegenForcesN, Is.EqualTo(new[] { 44000f, 0f, 44000f }));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Calculate_MotorCarRemovalUpdatesRegenTargets(int remainingMotorCarCount)
+        {
+            TimsBrakeContext context = CreateContext();
+            context.Input.cars[0].isVvvfMotorCar = true;
+            context.Input.cars[1].isVvvfMotorCar = true;
+            TimsBrakeNewLogic.Calculate(context);
+
+            context.Input.cars[0].isVvvfMotorCar = false;
+            context.Input.cars[1].isVvvfMotorCar = remainingMotorCarCount == 1;
+            TimsBrakeNewLogic.Calculate(context);
+
+            Assert.That(context.Workspace.targetRegenForcesN,
+                Is.EqualTo(new[] { 0f, remainingMotorCarCount == 1 ? 72000f : 0f }));
+        }
+
+        [Test]
+        public void Calculate_ReleaseClearsPreviousRegenTargets()
+        {
+            TimsBrakeContext context = CreateContext();
+            context.Input.cars[0].isVvvfMotorCar = true;
+            TimsBrakeNewLogic.Calculate(context);
+
+            context.Input.brakeStep = 0;
+            TimsBrakeNewLogic.Calculate(context);
+
+            Assert.That(context.Workspace.targetRegenForcesN, Is.EqualTo(new[] { 0f, 0f }));
+        }
+
         private static TimsBrakeContext CreateContext()
         {
             var context = new TimsBrakeContext();
