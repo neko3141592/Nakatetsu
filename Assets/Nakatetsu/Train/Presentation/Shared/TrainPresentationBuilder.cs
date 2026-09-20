1ad2b84f8b3d8338e5e02e0a1b3a6a504dd919bf
neko3141592 <yudai3.1415926@gmail.com>
2026-09-20 23:05:16 +0900

    Add train presentation builder scaffold

---
+++ Assets/Nakatetsu/Train/Presentation/Shared/TrainPresentationBuilder.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Nakatetsu.Train.Presentation.Shared
+{
+    public sealed class TrainPresentationBuilder : MonoBehaviour
+    {
+        [SerializeField] private TrainRoot trainRoot;
+
+        private void Awake()
+        {
+            // TODO: グラフができたら作成
+        }
+        
+    }
+}
