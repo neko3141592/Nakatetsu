56df38329de4a2d9bec0ec2a180775c844a4fa42
neko3141592 <yudai3.1415926@gmail.com>
2026-09-24 02:11:51 +0900

    Generate Series1000 train presentations
    - Instantiate consist car presentation prefabs from train definitions
    - Add Series1000 car data and M' body prefab with presentation links
    - Wire door and bogie presentation components into Series1000 bodies
    - Replace the debug presentation spawner with runtime presentation building
    - Add a local dot matrix glyph editor tool

--- Assets/Nakatetsu/Train/Composition/Scripts/TrainRoot.cs
+++ Assets/Nakatetsu/Train/Composition/Scripts/TrainRoot.cs
@@ -6,10 +6,17 @@ namespace Nakatetsu.Train
     [DisallowMultipleComponent]
     public sealed class TrainRoot : MonoBehaviour
     {
+        [SerializeField] private string trainId;
         [SerializeField] private ConsistDefinitionAsset consistDefinition;
 
+        public string TrainId => trainId;
         public ConsistDefinitionAsset ConsistDefinition => consistDefinition;
         public bool HasConsistDefinition => consistDefinition != null;
+
+        public void SetTrainId(string id)
+        {
+            trainId = id;
+        }
 
         public void Configure(ConsistDefinitionAsset definition)
         {
